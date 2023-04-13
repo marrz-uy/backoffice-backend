ef5660c26180db1c72832a7c5aa53fbc14f89f82
martinrodfl <martinrodf@gmail.com>
2023-04-13 18:29:27 -0300

    Mensajes push funcionando en backoffice

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//? VISTA DE MENSAJES */
+Route::get('/message', function () {
+    return view('message-form');
+})->name('message-form');
