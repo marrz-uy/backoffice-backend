bdb66efa4a812dcbcc214be2bf4ae3dec4664f1e
martinrodfl <martinrodf@gmail.com>
2023-07-04 00:01:25 -0300

    Arreglo para notificaciones

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,14 @@
 <?php
 
-use Illuminate\Http\Request;
 use App\Events\ClientNotifications;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-use App\Http\Controllers\TourController;
-use App\Http\Controllers\LoginController;
-
 use App\Http\Controllers\EventosController;
-use App\Http\Controllers\PuntosInteresController;
 use App\Http\Controllers\ImagenesPuntoInteresController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PuntosInteresController;
+use App\Http\Controllers\TourController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
@@ -34,27 +33,27 @@ Route::get('/Eventos', [EventosController::class, 'show'])->middleware("api");
 Route::post('/Eventos', [EventosController::class, 'store']);
 Route::delete('/Eventos/{id}', [EventosController::class, 'destroy']);
 Route::patch('/Eventos/{id}', [EventosController::class, 'update']);
-Route::post('/Eventos/{id}',[EventosController::class, 'ModificarImagenesEventos']);
+Route::post('/Eventos/{id}', [EventosController::class, 'ModificarImagenesEventos']);
 
 //TOUR PREDEFINIDO-------------------------------------------------------------------------------------------------------------------------->
 Route::POST('/tourPredefinido', [TourController::class, 'InsertarTourPredefinido']);
 Route::GET('/tourPredefinido', [TourController::class, 'ListarToursPredefinidos']);
-Route::PATCH('tourPredefinido',[TourController::class,'ModificarTourPredefinido']);
+Route::PATCH('tourPredefinido', [TourController::class, 'ModificarTourPredefinido']);
 Route::DELETE('/tourPredefinido/{i}', [TourController::class, 'destroy']);
-Route::POST('/tourPredefinido/{id}',[TourController::class, 'ModificarImagenesTour']);
+Route::POST('/tourPredefinido/{id}', [TourController::class, 'ModificarImagenesTour']);
 
 //AUXILIARES-------------------------------------------------------------------------------------------------------------------------->
 Route::group([
 
     'middleware' => 'api',
-    'prefix' => 'auth'
+    'prefix'     => 'auth',
 
 ], function ($router) {
-    Route::post('login','App\Http\Controllers\AuthController@login');
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
     //Route::post('login', [AuthController::class,'login']);
-    Route::post('logout',[AuthController::class,'logout']);
-    Route::post('refresh', [AuthController::class,'refresh']);
-    Route::post('me', [AuthController::class,'me']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
 
 });
 
